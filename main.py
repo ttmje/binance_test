05bdfbca604e32805529af9ec69d7fe68ec59667
ttmje <ivanttmyurlov@gmail.com>
2022-07-03 22:50:31 +0300

    added bookticker data, BT and OB data collected in txt file

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import json
 import requests
 from datetime import datetime
 
-now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S') # метка времени в формате год-месяц-день часы-минуты-секунды
 
 class Client():
 
     def get_snapshot(self):
-        url = "https://api.binance.com/api/v3/depth?symbol=FIROUSDT&limit=10"
+        url = "https://api.binance.com/api/v3/depth?symbol=MATICUSDT&limit=10"
         self.r = requests.get(url)
         self.orderbook = json.loads(self.r.content)
         self.LastUpdateID = self.orderbook['lastUpdateId']
@@ -20,36 +20,46 @@ class Client():
            file.write(self.r.text)
 
     async def depth(self):
-        urlws = "wss://stream.binance.com:9443/stream?streams=firousdt@depth@1000ms"
+        urlws = "wss://stream.binance.com:9443/stream?streams=maticusdt@depth@100ms"
         async with websockets.connect(urlws) as websocket:
             task.get_snapshot()
-            task.save_orderbook()
+            #task.save_orderbook()
             while True:
                data = json.loads(await websocket.recv())['data']
                if data['U'] <=  self.orderbook['lastUpdateId'] + 1 and data['u'] >=  self.orderbook['lastUpdateId'] + 1:
                    self.orderbook['lastUpdateId'] = data['u']
                    self.update_order_book(data)
-                   #print(self.data)
                else:
                    print('discard update')
 
-    def update_order_book(self, data):
-        for update in data['b']:
-            self.manage_orderbook('bids', update)
-        print('Best bids: ', self.orderbook['bids'][0])
-        for update in data['a']:
-            self.manage_orderbook('asks', update)
-        print('Best asks: ', self.orderbook['asks'][0])
-        #print('Spread: ')
+    async def bookticker(self):
+        urlbt = "wss://stream.binance.com:9443/stream?streams=maticusdt@bookTicker"
+        async with websockets.connect(urlbt) as websocket2:
+            with open(f'BookTickerDATA_{now}.txt', "a") as file:
+                while True:
+                    self.data2 = json.loads(await websocket2.recv())['data']
+                    print('Book Ticker best bids: ', self.data2['b'], self.data2['B'], "Last update id: ", self.data2['u'])
+                    print('Book Ticker best asks: ', self.data2['a'], self.data2['A'], "Last update id: ", self.data2['u'])
+                    file.writelines(f"best ask: {self.data2['a']}, best bid: {self.data2['b']}, Last update id: {self.data2['u']} \n")
 
+
+    def update_order_book(self, data):
+        with open(f'OrderBookData_{now}.txt', "a") as file:
+            for update in data['b']:
+                self.manage_orderbook('bids', update)
+            print('Order book best bids: ', self.orderbook['bids'][0],  "Last update id: ", data['u'])
+            for update in data['a']:
+                self.manage_orderbook('asks', update)
+            print('Order book best asks: ', self.orderbook['asks'][0], "Last update id: ", data['u'])
+            file.writelines(f"best ask: {self.orderbook['asks'][0]}, best bid: {self.orderbook['bids'][0]}, Last update id: {data['u']} \n")
 
     def manage_orderbook(self, side, update):
         price, qty = update
-        # loop through orderbook side
+        # проходимся по всему ордербуку
         for x in range(0, len(self.orderbook[side])):
             if price == self.orderbook[side][x][0]:
-                # when qty is 0 remove from orderbook, else
-                # update values
+                # если количество 0 - удаляем
+                # иначе - делаем апдейт
                 if float(qty) == 0:
                     self.orderbook[side].pop(x)
                     #print(f'Removed {price} {qty}')
@@ -65,7 +75,10 @@ class Client():
             else:
                 self.orderbook[side] = sorted(self.orderbook[side], key=lambda m: float(m[0]), reverse=True)
 
+    async def both(self):
+        await asyncio.gather(task.bookticker(), task.depth())
+
 if __name__ == '__main__':
     task = Client()
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(task.depth())
+    asyncio.run(task.both())
+
